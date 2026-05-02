8f1dc23988ac9f49e71a1506710d10eee2e895b1
NavinduAmerasinghe <navindu.amerasinghe@tuni.fi>
2026-05-02 08:46:26 +0300

    file arrangements done

--- dashboard/src/components/Sidebar.tsx
+++ dashboard/src/components/Sidebar.tsx
@@ -11,8 +11,6 @@ export default function Sidebar() {
   return (
     <div className="w-[22%] bg-[#1e1e2f] text-white flex flex-col text-[20px]">
       <nav>
-        <NavLink className={navLinkClass} to="/" end>Home Dashboard Overview</NavLink>
-        <NavLink className={navLinkClass} to="/location" end>Location-Based Insights</NavLink>
         <NavLink className={navLinkClass} to="/analytics" end>Analytics & Route Prediction</NavLink>
         <NavLink className={navLinkClass} to="/liveTraffic" end>Smart Road Weather & Traffic Pred</NavLink>
       </nav>
