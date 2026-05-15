90d45fdbdf3a755355e9b02f10e01ad12f2cdd2e
NavinduAmerasinghe <navindu.amerasinghe@tuni.fi>
2026-05-15 15:01:20 +0300

    create a render.yml file

--- backend/run_api.py
+++ backend/run_api.py
@@ -1,7 +1,12 @@
-"""
-Entry point to run the FastAPI server for wildlife collision risk prediction.
-"""
+import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("api.app:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run(
+        "api.app:app",
+        host="0.0.0.0",
+        port=port,
+        reload=False
+    )
